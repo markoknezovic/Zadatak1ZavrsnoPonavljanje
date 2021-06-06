12997f026735afc8fd079c5f5db22347cc63dd65
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-06 16:42:04 +0200

    Add files via upload

---
+++ Zadatak1(15posto)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unesi, sVelika, sMala, sPrvaTri, sZadnjihPet, s8_11;
+            Console.Write("Upisi nesto: ");
+            unesi = Console.ReadLine();
+            
+            sVelika = unesi.ToUpper();
+            Console.WriteLine(sVelika);
+
+            sMala = unesi.ToLower();
+            Console.WriteLine(sMala);
+
+            sPrvaTri = unesi.Substring(0, 3);
+            Console.WriteLine(sPrvaTri);
+
+            sZadnjihPet = unesi.Substring(unesi.Length - 5);
+            Console.WriteLine(sZadnjihPet);
+
+            s8_11 = unesi.Substring(7, 4);
+            Console.WriteLine(s8_11);
+
+            Console.ReadKey();
+
+        }
+    }
+}
